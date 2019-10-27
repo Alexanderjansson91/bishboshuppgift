7cde1302faa8d13889db831dda274e365babab9f
Alexander Jansson <Alexander_jansson_91@hotmail.com>
2019-10-27 19:18:13 +0100

    första pushen
    första pushen

---
+++ bischbosch.js
@@ -0,0 +1,65 @@
+function myFunction2() {
+
+    var x = document.getElementById("frm1"); // Läser in dom valda talen
+    var loopValue = x.elements[0].value; //variabel till värden
+    var division1 = x.elements[1].value;//skapar variabel till Bisch
+    var division2 = x.elements[2].value;//skapar varibel till Bosch
+    var text=""; 
+    var i;
+    for (i = 1; i <= loopValue; i++) {   //skapar en for loop av värderna
+
+        //  Skriver ut när siffran är både bisch och borsch
+        if (i % division1 == 0 && i % division2 == 0) { 
+
+          text += "Bisch-Bosch" + ", "; 
+
+          // Skriver ut när siffran är ett bisch tal
+        } else if (i % division1 == 0) { 
+            text+="Bisch" + ", "; 
+          
+        //  Skriver ut när siffran är ett Bosch tal
+        } else if (i % division2 == 0) { 
+            text+="Bosch" + ", "; 
+
+
+        } else {
+          text+=i + ", ";  //Skriver ut en siffra om det varken är bish eller bosch
+
+           
+        }
+
+    }
+    document.getElementById("demo").innerHTML = text; //Läser av html document. 
+
+
+}
+
+//scrollbar funktion för att välja antal
+function ti() {
+    var fP = document.getElementById('antalBB').innerHTML;
+    var tP = document.getElementById('antalBB').innerHTML;
+
+    if (fP != '' && tP != '')
+        window.location.replace(window.location.href + '?min_antal=' + fP + '&max_antal=' + tP);
+}
+//scrollbar funktion för att välja Bisch
+function ti() {
+    var Bi1 = document.getElementById('bischrange').innerHTML;
+    var bi2 = document.getElementById('bischrange').innerHTML;
+
+    if (Bi1 != '' && bi2 != '')
+        window.location.replace(window.location.href + '?min_brange=' + Bi1 + '&max_brange=' + bi2);
+}
+//scrollbar funktion för att välja Bosch
+function ti() {
+    var Bo1 = document.getElementById('boschrange').innerHTML;
+    var bo2 = document.getElementById('boschrange').innerHTML;
+
+    if (Bo1 != '' && bo2 != '')
+        window.location.replace(window.location.href + '?min_borange=' + Bo1 + '&max_borange=' + bo2);
+}
+//mouseover funktion  
+function scaleUp(obj, size) {
+    obj.style.fontSize = size;
+}
+
